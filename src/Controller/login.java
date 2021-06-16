445b2bc709bcb9749c9f1cf021fee4e5eb4e40ce
temuulenj <524101765@qq.com>
2021-06-16 09:12:09 +0800

    .

--- src/Controller/login.java
+++ src/Controller/login.java
@@ -12,12 +12,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.util.Objects;
 
 public class login extends Controller{
 
